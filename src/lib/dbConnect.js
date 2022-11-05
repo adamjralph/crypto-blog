f16465e9a44e010de8a105ffba666cc86893d8f6
Adam Ralph <adamralph@posteo.net>
2022-11-06 00:39:23 +1100

    articles working

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,33 +1,33 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb"
 
 if (!import.meta.env.ATLAS_URI) {
-  throw new Erro("Invalid: ATLAS_URI");
+    throw new Erro("Invalid: ATLAS_URI")
 }
 
-const uri = import.meta.env.ATLAS_URI;
-const dbName = import.meta.env.DB_NAME;
-const options = {};
-let cachedMongo;
+const uri = import.meta.env.ATLAS_URI
+const dbName = import.meta.env.DB_NAME
+const options = {}
+let cachedMongo
 
 const connectToAtlas = async () => {
-  const mongo = await new MongoClient(uri, options).connect();
-  return mongo.db(`${dbName}`);
-};
+    const mongo = await new MongoClient(uri, options).connect()
+    return mongo.db(`${dbName}`)
+}
 
 export const getDb = async () => {
-  if (import.meta.env.NODE_ENV === "development") {
-    if (!global._mongoConnection) {
-      global._mongoConnection = await connectToAtlas();
-      cachedMongo = global._mongoConnection;
+    if (import.meta.env.NODE_ENV === "development") {
+        if (!global._mongoConnection) {
+            global._mongoConnection = await connectToAtlas()
+            cachedMongo = global._mongoConnection
+        }
+        return cachedMongo
     }
-    return cachedMongo;
-  }
-  const mongo = await connectToAtlas();
-  return mongo;
-};
+    const mongo = await connectToAtlas()
+    return mongo
+}
 
 export const Posts = async () => {
-  const db = await getDb();
-  console.log("Connected to database...");
-  return db.collection("post");
-};
+    const db = await getDb()
+    console.log("Connected to database...")
+    return db.collection("post")
+}
