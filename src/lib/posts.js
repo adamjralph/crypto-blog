95ebcbe46d4235843048f9c3781116fc442b54d1
Adam Ralph <adamralph@posteo.net>
2022-11-06 10:39:21 +1100

    updates

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,11 +1,16 @@
-import { Posts } from "./dbConnect";
+import { Posts } from "./dbConnect"
 
 export const getAllPosts = async () => {
-  const posts = await (await Posts()).find().toArray();
-  return posts;
-};
+    const posts = await (await Posts()).find().toArray()
+    return posts
+}
+
+export const getImageLink = async () => {
+    const image = await (await Posts()).distinct("image")
+    return image
+}
 
 export const createPost = async (newPost) => {
-  const post = await (await Posts()).insert(newPost);
-  return post;
-};
+    const post = await (await Posts()).insert(newPost)
+    return post
+}
